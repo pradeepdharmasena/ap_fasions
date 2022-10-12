6044284a4aefa79c467de366f38d68bd0c084005
pradeep <dharmasenatap@gmail.com>
2022-10-12 22:13:21 +0530

    create dummy few dummy pages and implement routing functions

---
+++ src/Pages/HatPage/HatPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './HatPage.scss'
+
+export const HatPage = () =>
+    (
+        <div>
+            <h1> this is hat page</h1>
+            {console.log("hats")}
+        </div>
+    )
